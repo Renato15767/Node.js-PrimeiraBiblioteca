bcf95b2ba4f4d021f006b99c7a4b66c56666b55a
Renato15767 <renatominoru46@gmail.com>
2023-06-19 21:20:11 -0300

    Aprendendo a lib fs e tratamento de erros

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+//Importa chalk (precisa usar o NPM)
 import chalk from 'chalk';
-//encadear métodos para colorir texto, cor de fundo e texto em negrito
-console.log(chalk.blue.bgWhite.bold('Alura'));
-//receber múltiplos argumentos
-console.log(chalk.blue('curso', 'de', 'Node.js'));
-//métodos aninhados
-console.log(chalk.red('vermelho', chalk.underline.bgBlue('azul')));
-// uso de template strings e placeholders
-console.log(`
-CPU: ${chalk.red('90%')}
-RAM: ${chalk.green('40%')}
-DISK: ${chalk.yellow('70%')}
-`);
+//FS é uma lib nativa do node.js, que permite acessar outros arq.
+import fs from 'fs'
+
+function TrataErro(erro){
+    console.log(erro)
+    //irá lançar o erro no terminal para ser tratado
+    throw new Error(chalk.red(erro.code, "não há arquivo no diretório"))
+}
+
+function PegaArquivo(caminhoArq){
+    const enconding = 'utf-8'
+
+    //Lê o arquivo e pega seu conteúdo
+    fs.readFile(caminhoArq, enconding, (erro, texto) =>{
+        //Caso tenha algum erro, irá cair no if
+        if(erro){
+            TrataErro(erro)
+        }
+        console.log(chalk.green(texto))
+    })
+}
+
+// ./ pega apartir do diretório que estamos trabalhando
+PegaArquivo("./arquivos/texto.md")
