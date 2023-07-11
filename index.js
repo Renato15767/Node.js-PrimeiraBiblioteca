584048dde3e98cb6e10021e1a20fee725e664963
Renato15767 <renatominoru46@gmail.com>
2023-07-11 20:46:56 -0300

    Aula 2 Concluída - Promessa e async/await utilizados

--- index.js
+++ index.js
@@ -9,18 +9,42 @@ function TrataErro(erro){
     throw new Error(chalk.red(erro.code, "não há arquivo no diretório"))
 }
 
-function PegaArquivo(caminhoArq){
-    const enconding = 'utf-8'
 
-    //Lê o arquivo e pega seu conteúdo
-    fs.readFile(caminhoArq, enconding, (erro, texto) =>{
-        //Caso tenha algum erro, irá cair no if
-        if(erro){
-            TrataErro(erro)
-        }
+// async/await
+
+// async deixa a função assíncrona
+async function PegaArquivo(caminhoArq){
+    //Try é executado primeiro
+    try{
+        const enconding = 'utf-8'
+        // Faz com que o trecho do cód. aguarde o retorno
+        const texto = await fs.promises.readFile(caminhoArq, enconding)
         console.log(chalk.green(texto))
-    })
+        //catch pega os erros
+    }catch (erro){
+        TrataErro(erro)
+        //finally sempre é executado
+    }finally{
+        console.log(chalk.yellow('operação concluída'));
+    }
+    
 }
+
+
+// promises com then()
+
+// function PegaArquivo(caminhoArq){
+//     const enconding = 'utf-8'
+//     //promises = código assícrono. Ele promete que irá fazer
+//     fs.promises.readFile(caminhoArq, enconding)
+//         //then encadea código assíncrono, ou seja, faz uma coisa e manda fazer outra ao mesmo tempo.
+//         //texto é o arq. que vai ser recebido
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(TrataErro)
+// }
+
+
 
 // ./ pega apartir do diretório que estamos trabalhando
 PegaArquivo("./arquivos/texto.md")
+PegaArquivo("./arquivos/")
