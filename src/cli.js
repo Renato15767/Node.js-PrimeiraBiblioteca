32762f1c5a584ac12d18f32cce3a73fd7761c144
Renato15767 <renatominoru46@gmail.com>
2023-07-19 23:04:43 -0300

    Seção 4 Terminada - Foi aprendido a como tratar erros, processar arquivos com comandos assíncronos

--- src/cli.js
+++ src/cli.js
@@ -7,16 +7,40 @@ import PegaArquivo from "./index.js"
 // na linha de comando para dentro do programa
 const caminho = process.argv
 
+function ImprimiLista(resultado, nomeArquivo = ''){
+    console.log(chalk.yellow('Lista de Links'),
+    chalk.black.bgBlue(nomeArquivo),
+    resultado)
+}
+
 //Função precisa ser assícrona, por conta da outra
 async function ProcessaArquivo(argumentos){
     const caminho = argumentos[2]
 
+    try{
+        fs.lstatSync(caminho)
+    }catch(erro){
+        if(erro.code === 'ENOENT'){
+            console.log('Arquivo ou diretório não existem')
+            return
+        }
+    }
+
     // Verifica se o caminho é de um arquivo
     if(lstatSync(caminho).isFile()){
         const resultado = await PegaArquivo(argumentos[2])
-        console.log(chalk.yellow('Lista de Links'), resultado)
-    }
+        ImprimiLista(resultado)
 
+    }else if(lstatSync(caminho).isDirectory()){
+        // Código assíncrono e lê um diretório
+        const arquivos = await fs.promises.readdir(caminho)
+        // o forEach é assíncrono
+        arquivos.forEach(async (nomeArquivo) => {
+            // Manda caminho do arquivo para a Func. PegaArquivo
+            const lista = await PegaArquivo(`${caminho}/${nomeArquivo}`)
+            ImprimiLista(lista, nomeArquivo)
+        })
+    }
 }
 
 ProcessaArquivo(caminho)
